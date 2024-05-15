e38c9d5d9281f4d55bfde486232cfbe24ee7142d
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-05-15 23:47:11 +0000

    done with p1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,9 +16,10 @@ class Movie < ApplicationRecord
 
   def director
     find_director_id= self.director_id
-    director=Director.where(:id => "find_director_id").first
+    director=Director.where(:id => find_director_id).first
    
-    return find_director
- 
+    return director
+
    end   
+
 end
